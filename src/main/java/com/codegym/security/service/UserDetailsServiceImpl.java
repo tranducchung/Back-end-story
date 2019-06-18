6fddcba5d84dffdbc0be04d7d0254b27cedea925
nguyenanh <nguyenanhtran2406@gmail.com>
2019-06-18 14:55:27 +0700

    nguyenanh cr sv

--- src/main/java/com/codegym/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/codegym/security/service/UserDetailsServiceImpl.java
@@ -1,7 +1,7 @@
 package com.codegym.security.service;
 
 import com.codegym.model.User;
-import com.codegym.repository.UserRepository;
+import com.codegym.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,13 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        User user = userRepository.findByEmail(username).orElseThrow(
+        User user = userService.findByEmail(username).orElseThrow(
                 () -> new UsernameNotFoundException("User Not Found with -> username or email : " + username));
 
         return UserPrinciple.build(user);
