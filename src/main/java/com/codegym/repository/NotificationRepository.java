c7bbd45fc13c3d885f08439d0396dcc7a5334c98
nbthanh98 <nguyenbathanh1280@gmail.com>
2019-06-28 16:23:48 +0700

    Thanh: add api notification

--- src/main/java/com/codegym/repository/NotificationRepository.java
+++ src/main/java/com/codegym/repository/NotificationRepository.java
@@ -1,9 +1,21 @@
 package com.codegym.repository;
 
 import com.codegym.model.Notification;
+import com.codegym.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
+@Transactional
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    @Query("SELECT n FROM Notification n INNER JOIN n.userReceive u WHERE u.id=?1 ORDER BY n.id DESC ")
+    List<Notification> findAllNotificationByUserIdAndOderById(Long id);
+
+    void deleteAllByUserReceive(User user);
+
 }
--- src/main/java/com/codegym/service/NotificationService.java
+++ src/main/java/com/codegym/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.codegym.service;
 
 import com.codegym.model.Notification;
+import com.codegym.model.User;
 
 import java.util.List;
 
@@ -8,4 +9,12 @@ public interface NotificationService {
     void save(Notification notification);
     List<Notification> findAll();
     void delete(Notification notification);
+
+    List<Notification> findAllNotificationByUserIdAndOderById(Long id);
+
+    void deleteAllNotificationByUser(User user);
+
+    // delete notification by id
+
+    void deleteNotificationById(Long id);
 }
