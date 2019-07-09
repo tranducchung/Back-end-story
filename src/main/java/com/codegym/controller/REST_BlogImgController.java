9325ff5581002c4439f345739da3e17e31ad14c6
nbthanh98 <nbthanh98>
2019-07-10 02:18:25 +0700

    demo api album img

---
+++ src/main/java/com/codegym/controller/REST_BlogImgController.java
@@ -0,0 +1,77 @@
+package com.codegym.controller;
+
+import com.codegym.model.BlogImg;
+import com.codegym.model.MyUpload;
+import com.codegym.model.User;
+import com.codegym.security.service.UserPrinciple;
+import com.codegym.service.BlogImgService;
+import com.codegym.service.MyUpLoadService;
+import com.codegym.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class REST_BlogImgController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BlogImgService blogImgService;
+
+    @Autowired
+    private MyUpLoadService myUpLoadService;
+
+    @PostMapping(value = "/api/blogimgs",consumes = { "multipart/mixed", "multipart/form-data" })
+    public ResponseEntity<Void> createBlogImg(@RequestParam("data") String blogImg, @RequestParam("file") MultipartFile file) {
+        if (blogImg == null || file == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        MyUpload myUpload = new MyUpload();
+        String fileName = ramdom() + file.getOriginalFilename();
+        myUpload.setSrcImg(fileName);
+        myUpload.setUser(getUserFromToken());
+        myUpLoadService.save(myUpload);
+        myUpLoadService.store(file, fileName);
+        List<MyUpload> listUpload = new ArrayList<>();
+        listUpload.add(myUpload);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            BlogImg blogImg2 = mapper.readValue(blogImg, BlogImg.class);
+            blogImg2.setListImg(listUpload);
+            blogImg2.setUser(getUserFromToken());
+            blogImgService.save(blogImg2);
+            System.out.println(blogImg2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    private User getUserFromToken() {
+        Object authen = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long id_user = ((UserPrinciple)authen).getId();
+        User user = userService.findUserByID(id_user);
+        return user;
+    }
+
+    private static Long ramdom() {
+        return (long) Math.floor((Math.random() * 1000000));
+    }
+}
---
+++ src/main/java/com/codegym/service/BlogImgService.java
@@ -0,0 +1,7 @@
+package com.codegym.service;
+
+import com.codegym.model.BlogImg;
+
+public interface BlogImgService {
+    void save(BlogImg blogImg);
+}
